b069db0d1fd8bcd61d8521de2bac03f1322ccd88
tanbr <RealTanbro@Gmail.com>
2017-03-09 16:13:31 +0800

    add: book.js configuration file

---
+++ book.js
@@ -0,0 +1,30 @@
+var pkg = require('./package.json');
+
+module.exports = {
+    title: "快速入门",
+    author: "ONEYUN Team",
+    language: "zh",
+    gitbook: '3.2.2',
+    plugins: [
+        '-sharing',
+        'toc',
+        'codetabs',
+        "theme-api"
+    ],
+    variables: {
+        version: pkg.version
+    },
+    pluginsConfig: {
+        "theme-api": {
+            "theme": "dark",
+            "languages": [{
+                "lang": "java",
+                "name": "JAVA",
+                "default": true
+            }, {
+                "lang": "php",
+                "name": "PHP"
+            }]
+        }
+    }
+};
